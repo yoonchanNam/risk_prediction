23d1673cd401c2c192a5014421c198798922ddc3
yoonchanNam <71334145+yoonchanNam@users.noreply.github.com>
2021-07-04 14:38:06 +0900

    Update risk_prediction.py

--- risk_prediction.py
+++ risk_prediction.py
@@ -73,9 +73,10 @@ def draw_boxes(region,dx,dy,ds,img, bbox, identities=None, categories=None, name
         
         w = x2 - x1
         h = y2 - y1
-         
+        
+        ratio = h/w
         # box 크기 변화율에 따라 근사화 하여 다음 box의 크기를 결정
-        dx1 = round(int(ds[id-1])/(2*w + 1.6*h))
+        dx1 = round(int(ds[id-1])/(2*w + 2*raito*h))
         dy1 = round(0.8*dx1)
 
 
